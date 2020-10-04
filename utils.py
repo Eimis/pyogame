a5f8c4f55dbd25324db4fdc6afc24e86e197c629
Eimantas Stonys <eimantas.stonys@finqle.nl>
2020-10-04 10:17:54 +0300

    Add Celery and Rabbit

--- utils.py
+++ utils.py
@@ -1,9 +1,27 @@
+from datetime import timedelta
 import os
+
 from ogame import OGame
 from ogame.constants import coordinates, ships, mission, speed
 
+from celery import Celery
+from celery.schedules import crontab
 
-def send_expedition():
+app = Celery('tasks', broker=os.environ.get('CELERY_BROKER_URL'))
+
+app.conf.beat_schedule = {
+    'send-expeditions': {
+        'task': 'utils.send_expeditions',
+        'schedule': crontab(minute='*/15'),  # every 15 mins
+    },
+}
+app.conf.timezone = 'Europe/Vilnius'
+
+logger = get_task_logger(__name__)
+
+
+@app.task
+def send_expeditions():
 
     empire = OGame(
         'Leda',
@@ -27,6 +45,8 @@ def send_expedition():
                 bool(planet_ships.large_transporter.amount)
                 and bool(planet_ships.explorer.amount)
             ):
+
+                logger.info('Attempting to send Expedition for planet: '.format(planet))
 
                 empire.send_fleet(
                     mission=mission.expedition,
